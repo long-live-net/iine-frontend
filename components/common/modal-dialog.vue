ddeda00415c3cd36291f58161a5e8fba8b5676b4
ngzm <nk.ngzm@gmail.com>
2023-12-22 15:02:02 +0900

    問い合わせダイアログスクロールとparsistant対応

--- components/common/modal-dialog.vue
+++ components/common/modal-dialog.vue
@@ -6,12 +6,14 @@ const props = withDefaults(
     titleIcon?: string
     titleIconColor?: string
     maxWidth?: number
+    persistent?: boolean
   }>(),
   {
     title: '設定ダイアログ',
     titleIcon: 'mdi-cog',
     titleIconColor: 'primary',
     maxWidth: 600,
+    persistent: false,
   }
 )
 const emit = defineEmits<{
@@ -27,10 +29,14 @@ const dialog = computed({
 </script>
 
 <template>
-  <BaseDialog v-model:modal="dialog" :max-width="maxWidth">
-    <div class="dialog-base-frame">
-      <div class="modal-dialog">
-        <header class="modal-dialog__header">
+  <BaseDialog
+    v-model:modal="dialog"
+    :max-width="maxWidth"
+    :persistent="persistent"
+  >
+    <div class="modal-dialog">
+      <header class="modal-dialog__header">
+        <div class="header-label">
           <slot name="header">
             <p class="mr-1">
               <v-icon
@@ -41,20 +47,20 @@ const dialog = computed({
             </p>
             <h3>{{ title }}</h3>
           </slot>
-        </header>
-        <div class="modal-dialog__body">
-          <slot />
         </div>
-      </div>
-      <div class="dismiss">
-        <v-btn
-          variant="text"
-          append-icon="mdi-close"
-          size="small"
-          color="primary"
-          @click="dialog = false"
-          >閉じる</v-btn
-        >
+        <div class="dismiss">
+          <v-btn
+            variant="text"
+            append-icon="mdi-close"
+            size="small"
+            color="primary"
+            @click="dialog = false"
+            >閉じる</v-btn
+          >
+        </div>
+      </header>
+      <div class="modal-dialog__body">
+        <slot />
       </div>
     </div>
   </BaseDialog>
@@ -69,21 +75,17 @@ const dialog = computed({
   &__header {
     display: flex;
     align-items: center;
+    justify-content: space-between;
     padding: 1rem;
+    padding-right: 6px;
     border-bottom: 1px solid lightgray;
+    .header-label {
+      display: flex;
+      align-items: center;
+    }
   }
   &__body {
     padding: 1.5rem;
-  }
-}
-
-.dialog-base-frame {
-  position: relative;
-
-  .dismiss {
-    position: absolute;
-    top: 20px;
-    right: 8px;
   }
 }
 </style>
