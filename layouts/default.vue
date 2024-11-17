8909ee9a3e9b6896520af95c7f123adea5127e75
ngzm <nk.ngzm@gmail.com>
2024-11-17 21:25:11 +0900

    ページ表示崩れ修正して Realease V1.1.4

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,14 +27,16 @@ onMounted(() => {
   if (initializing.value)
     setTimeout(() => {
       initializing.value = false
-    }, 300)
+    }, 1000)
 })
 </script>
 
 <template>
-  <div id="default-layout-container">
-    <div v-if="initializing" class="initializing">loading ...</div>
-    <div v-show="!initializing">
+  <div id="default-layout">
+    <div v-if="initializing" class="initializing">
+      <div class="loading">loading ...</div>
+    </div>
+    <div v-show="!initializing" id="default-layout-container">
       <header>
         <PublishMenuNavHeader />
       </header>
@@ -49,51 +51,59 @@ onMounted(() => {
 </template>
 
 <style lang="scss" scoped>
-#default-layout-container {
-  display: flex;
-  flex-flow: column;
-  min-height: 100dvh;
+#default-layout {
+  #default-layout-container {
+    min-height: 100dvh;
+    display: flex;
+    flex-flow: column;
+    align-items: stretch;
 
-  header {
-    position: fixed;
-    top: 0px;
-    width: 100%;
-    z-index: 999;
-  }
+    header {
+      position: fixed;
+      top: 0px;
+      width: 100%;
+      z-index: 999;
+    }
 
-  main {
-    flex-grow: 1;
-    position: relative;
-    margin: 0;
-    padding: 0;
-  }
+    main {
+      flex-grow: 1;
+      position: relative;
+      margin: 0;
+      padding: 0;
+    }
 
-  footer {
-    padding: 0;
-    margin: 0;
+    footer {
+      padding: 0;
+      margin: 0;
+    }
   }
 
   .initializing {
-    // TODO:
-    // 取り急ぎアニメーションは適当なので後日かっこいいものに変更したい
-    margin: auto;
-    font-family: sans-serif;
-    font-size: 3rem;
-    font-weight: bold;
-    animation: ini-scaleout 1.5s infinite linear;
+    min-height: 100dvh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-    @media only screen and (max-width: $grid-breakpoint-md) {
-      font-size: 2rem;
-    }
-  }
+    .loading {
+      // TODO:
+      // 取り急ぎアニメーションは適当なので後日かっこいいものに変更したい
+      font-family: sans-serif;
+      font-size: 3rem;
+      font-weight: bold;
+      animation: ini-scaleout 1.5s infinite linear;
 
-  @keyframes ini-scaleout {
-    0% {
-      transform: scale(0.25);
+      @media only screen and (max-width: $grid-breakpoint-md) {
+        font-size: 2rem;
+      }
     }
-    100% {
-      transform: scale(1);
-      opacity: 0.25;
+    @keyframes ini-scaleout {
+      0% {
+        transform: scale(0.25);
+      }
+      100% {
+        transform: scale(1);
+        opacity: 0.25;
+      }
     }
   }
 }
