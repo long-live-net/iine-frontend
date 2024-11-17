36fa7e70140384e6855af81965a7e115835dea91
ngzm <nk.ngzm@gmail.com>
2024-11-17 18:37:23 +0900

    初期画面でフェッチが完了してから表示するように対応した
    とりあえずLoading中のアニメーションは簡単版、後日格好良くしたい
    cf. #159

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,19 +21,30 @@ useHead({
     },
   ],
 })
+
+const initializing = useState<boolean>('initializing', () => true)
+onMounted(() => {
+  if (initializing.value)
+    setTimeout(() => {
+      initializing.value = false
+    }, 300)
+})
 </script>
 
 <template>
   <div id="default-layout-container">
-    <header>
-      <PublishMenuNavHeader />
-    </header>
-    <main>
-      <slot />
-    </main>
-    <footer>
-      <PublishMenuNavFooter />
-    </footer>
+    <div v-if="initializing" class="initializing">loading ...</div>
+    <div v-show="!initializing">
+      <header>
+        <PublishMenuNavHeader />
+      </header>
+      <main>
+        <slot />
+      </main>
+      <footer>
+        <PublishMenuNavFooter />
+      </footer>
+    </div>
   </div>
 </template>
 
@@ -42,21 +53,48 @@ useHead({
   display: flex;
   flex-flow: column;
   min-height: 100dvh;
+
   header {
     position: fixed;
     top: 0px;
     width: 100%;
     z-index: 999;
   }
+
   main {
     flex-grow: 1;
     position: relative;
     margin: 0;
     padding: 0;
   }
+
   footer {
     padding: 0;
     margin: 0;
+  }
+
+  .initializing {
+    // TODO:
+    // 取り急ぎアニメーションは適当なので後日かっこいいものに変更したい
+    margin: auto;
+    font-family: sans-serif;
+    font-size: 3rem;
+    font-weight: bold;
+    animation: ini-scaleout 1.5s infinite linear;
+
+    @media only screen and (max-width: $grid-breakpoint-md) {
+      font-size: 2rem;
+    }
+  }
+
+  @keyframes ini-scaleout {
+    0% {
+      transform: scale(0.25);
+    }
+    100% {
+      transform: scale(1);
+      opacity: 0.25;
+    }
   }
 }
 </style>
